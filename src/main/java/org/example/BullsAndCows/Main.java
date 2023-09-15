b6b4ea4b4d1a9e668e7d79f7440be1e69299693e
Yura <yura.tarasevichunt@gmail.com>
2023-09-15 17:12:38 +0300

    BullsAndCows2.0

--- src/main/java/org/example/BullsAndCows/Main.java
+++ src/main/java/org/example/BullsAndCows/Main.java
@@ -14,6 +14,33 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Выберите действие:");
+            System.out.println("1. Новая игра");
+            System.out.println("2. Посмотреть историю игр");
+            System.out.println("3. Выход");
+            System.out.print("Введите номер действия: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    playGame();
+                    break;
+                case 2:
+                    viewGameHistory();
+                    break;
+                case 3:
+                    System.out.println("Выход из программы.");
+                    break;
+                default:
+                    logger.log(Level.SEVERE, "Неправильный выбор.");
+                    System.out.println("Неправильный выбор. Попробуйте еще раз.");
+                    break;
+            }
+        } while (choice != 3);
+    }
+    private static void playGame() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Выберите игру:");
         System.out.println("1. Игра с числами");
         System.out.println("2. Английская игра");
@@ -36,17 +63,19 @@ public class Main {
                 System.out.println("Неправильный выбор. Завершение программы.");
                 return;
         }
-        game.start(5, 5);
+        System.out.println("Введите количество символов: ");
+        game.start(scanner.nextInt(), 5);
         scanner.nextLine(); // Очистка буфера после ввода номера игры
-        while (!game.getGameStatus().equals(GameStatus.WINNER) && !game.getGameStatus().equals(GameStatus.LOSE)) {
+        do {
             System.out.print("Введите значение: ");
             String scannerWord = scanner.nextLine();
             Answer answer = game.inputValue(scannerWord);
             logger.log(Level.INFO, "Введено значение: {0}, ответ: {1}", new Object[]{scannerWord, answer});
             System.out.println("Ответ: " + answer);
-        }
+        } while (!game.getGameStatus().equals(GameStatus.WINNER) && !game.getGameStatus().equals(GameStatus.LOSE));
         logger.log(Level.INFO, "Статус игры: {0}", game.getGameStatus());
         System.out.println("Статус игры: " + game.getGameStatus());
+        saveGameHistory(game); // Сохранение игры в файл
     }
     private static void viewGameHistory() {
         Scanner scanner = new Scanner(System.in);
@@ -68,19 +97,37 @@ public class Main {
             System.out.println("Ошибка чтения файла: " + fileName);
         }
     }
-
     private static void saveGameHistory(Game game) {
         Logger gameLogger = Logger.getLogger(game.getClass().getName());
         FileHandler fileHandler;
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите название файла для сохранения истории игры: ");
+        String fileName = "save";
         try {
-            fileHandler = new FileHandler(game.getClass().getName() + "_history.log", true);
+            fileHandler = new FileHandler(fileName + ".log", true);
             gameLogger.addHandler(fileHandler);
             SimpleFormatter formatter = new SimpleFormatter();
             fileHandler.setFormatter(formatter);
             gameLogger.log(Level.INFO, "Игра завершена со статусом: {0}", game.getGameStatus());
         } catch (IOException e) {
-                logger.log(Level.SEVERE, "Ошибка создания файла истории игры.", e);
+            logger.log(Level.SEVERE, "Ошибка создания файла истории игры.", e);
+            System.out.println("Ошибка создания файла истории игры.");
+        }
+        System.out.print("Показать историю игры? (y/n): ");
+        String showHistory = scanner.nextLine();
+        if (showHistory.equals("y")) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(fileName + ".log"));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Ошибка чтения файла истории игры.", e);
+                System.out.println("Ошибка чтения файла истории игры.");
             }
         }
-}
+    }
 
+}
